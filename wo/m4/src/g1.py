4196f7784c56bf0e847c2e28edaff92bf9f1cff2
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-05-19 12:52:59 +0000

    🫦

---
+++ wo/m4/src/g1.py
@@ -0,0 +1,28 @@
+import time
+import gradio as gr
+
+def slow_echo(message, history):
+    for i in range(len(message)):
+        time.sleep(0.05)
+        yield "You typed: " + message[: i + 1]
+
+def create_chat_interface(echo_function, **kwargs):
+    """Creates a Gradio ChatInterface with customizable parameters
+    
+    Args:
+        echo_function: The function that processes messages
+        **kwargs: Additional arguments for ChatInterface
+        
+    Returns:
+        Configured ChatInterface instance
+    """
+    defaults = {
+        "title": "Slow Booty Dance",
+        "type": "messages",
+        "flagging_mode": "manual",
+        "flagging_options": ["Like", "Spam", "Inappropriate", "Other"],
+        "save_history": True,
+    }
+    defaults.update(kwargs)
+    
+    return gr.ChatInterface(echo_function, **defaults)
