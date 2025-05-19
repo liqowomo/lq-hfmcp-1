aa0d204c081a67d868a0d9f7c638403308b22545
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-05-19 12:59:45 +0000

    🫦

--- wo/m4/main.py
+++ wo/m4/main.py
@@ -2,8 +2,10 @@
 # https://www.gradio.app/playground 
 
 from src.g1 import g1_main # Main gradio function herex
+from src.ver1 import ver1_main # Get Versions
 
 def main():
+    ver1_main()
     g1_main()
 
 
