7403cc8762a4849ad18acf90fc6ef6aad9054ec2
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-05-19 13:03:08 +0000

    🫦

--- wo/m4/src/ver1.py
+++ wo/m4/src/ver1.py
@@ -3,15 +3,17 @@
 
 from .utz import rprint
 
-import sys
-import pkg_resources
+import sys 
+import pkg_resources  # For pip version
 
 def get_versions():
-    python_version = sys.version
+    """Returns Python and pip versions as a tuple"""
+    python_version = sys.version.split()[0]  # Gets just the version number
     pip_version = pkg_resources.get_distribution("pip").version
     return python_version, pip_version
 
 def exec_get_versions():
-    python_version = get_versions()
-    rprint(f"Python version: {python_version}")
-    rprint(f"Pip version: {pip_version}")
+    """Executes and prints version information"""
+    python_version, pip_version = get_versions()  # Unpack the tuple
+    rprint(f"[bold]Python version:[/bold] {python_version}")
+    rprint(f"[bold]Pip version:[/bold] {pip_version}")
