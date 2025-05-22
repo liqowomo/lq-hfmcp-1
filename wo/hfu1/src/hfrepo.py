568d6101f2ed8e6797ee63f70a7863b21b23d8e8
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-05-22 17:41:15 +0000

    🫦

--- wo/hfu1/src/hfrepo.py
+++ wo/hfu1/src/hfrepo.py
@@ -2,8 +2,14 @@
 
 from src.utz import header1
 
+# Main function that will call the sub functions
 def hf_repo_ops():
     """
     Main function for Hugging Face repository operations.
     """
     header1("BootySmell")
+
+# --- Function for creating a repo --- 
+
+def hf_create_repo():
+    
