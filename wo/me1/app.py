1a3716067e9b5d8adbf55d940337a5b9ad64e521
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-05-23 14:30:29 +0000

    🫦

--- wo/me1/app.py
+++ wo/me1/app.py
@@ -1,20 +1,23 @@
 import gradio as gr
 
-MAIN_TEXT="""
+MAIN_TEXT = """
 # me1test1
 
 1. Testing out markdown in the tab
 """
 
-intro_page = gr.Interface(lambda: MAIN_TEST, inputs=None, outputs="markdown")
-hello_world = gr.Interface(lambda name: "Hello " + name, "text", "text")
-bye_world = gr.Interface(lambda name: "Bye " + name, "text", "text")
-chat = gr.ChatInterface(lambda *args: "Hello " + args[0])
+# Use Blocks to make a non-interactive markdown display
+with gr.Blocks() as intro_page:
+    gr.Markdown(MAIN_TEXT)
+
+hello_world = gr.Interface(fn=lambda name: "Hello " + name, inputs="text", outputs="text")
+bye_world = gr.Interface(fn=lambda name: "Bye " + name, inputs="text", outputs="text")
+chat = gr.ChatInterface(fn=lambda *args: "Hello " + args[0])
 
 demo = gr.TabbedInterface(
-    [intro_page, hello_world, bye_world, chat], 
-    ["Intro Page", "Hello World", "Bye World", "Chat"]
-    )
+    [intro_page, hello_world, bye_world, chat],
+    ["Intro Page", "Hello World", "Bye World", "Chat"],
+)
 
 if __name__ == "__main__":
     demo.launch()
