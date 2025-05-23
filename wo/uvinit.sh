d715a35df42c775ff87244ffebef1ecaf0dc0a50
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-05-23 12:49:56 +0000

    🫦

--- wo/uvinit.sh
+++ wo/uvinit.sh
@@ -85,37 +85,5 @@ uv_gradio_mcp() {
     echo -e "${GREEN}***** Installation Completed *****${NC}"
 }
 
-# UvSetup with gradio[mcp]
-uv_gradio() {
-    hea1 "UV Setup with Fast Agent"
-
-    # Get Name of project
-    echo -e "Enter the name of the project: "
-    read name_of_project
-    if [ -z "$name_of_project" ]; then
-        echo -e "${RED}BASTARD ! Project name cannot be empty${NC}"
-        exit 1
-    fi
-
-    # UC Commands
-    CO1="uv init $name_of_project"
-    CO2="cd $name_of_project"
-
-    DEPS="rich fast-agent-mcp "
-    CO3="uv add  $DEPS"
-    CO4="uv tree"
-
-    ## RUN Above Commands
-    echo -e "--- Executing ${CO1} ---"
-    eval "$CO1"
-    echo -e "--- Executing ${CO2} ---"
-    eval "$CO2"
-    echo -e "--- Executing ${CO3} ---"
-    eval "$CO3"
-    echo -e "--- Executing ${CO4} ---"
-    eval "$CO4"
-    echo -e "${GREEN}***** Installation Completed *****${NC}"
-}
-
 # Execution
-uvsetup_1
+uv_gradio_mcp
